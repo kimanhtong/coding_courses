f66a237dc7c12c0e6fe9d9137723df9e5e5e3c4d
Anh Tong <tongkimanh@yahoo.com>
2022-03-26 03:07:37 +0000

    To add default port for front end of package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import axios from "axios";
 import ProgramList from "./components/ProgramList";
 
-const API_URL = "http://localhost:3000/api/v1/programs";
+const program_list_url = "http://localhost:3000/api/v1/programs";
 
 
 const App = () => {
   const [programs, setPrograms] = useState([{id:0,name:"",description:"",duration_days:0}]);
   const fetchPrograms = () => {
-    axios.get(API_URL)
+    axios.get(program_list_url)
     .then (res => {
       setPrograms(res.data)})
     .catch(res => console.log(res.message));
