123b45304661b2f5183f5c5e8ac317ea897eddc7
Anh Tong <tongkimanh@yahoo.com>
2022-03-28 04:07:06 +0000

    To update content of Program List

--- frontend/src/components/Program/ProgramList.jsx
+++ frontend/src/components/Program/ProgramList.jsx
@@ -4,13 +4,23 @@ import ProgramsContext from '../../context/ProgramsContext';
 
 const ProgramList = () => {
   const { programs, setPrograms } = useContext(ProgramsContext);
-  const program_list_url = "http://localhost:3000/api/v1/programs";
+  ;
   const fetchPrograms = () => {
+    const program_list_url = "http://localhost:3000/api/v1/programs"
     axios.get(program_list_url)
     .then (res => {
       setPrograms(res.data)})
     .catch(res => console.log(res.message));
   };
+
+  const deleteProgram =  (id) => {
+    const program_delete_url = `http://localhost:3000/api/v1/programs/${id}`
+    axios.delete(program_delete_url)
+    .then (res => {
+      setPrograms(res.data)})
+    .catch(res => console.log(res.message))
+  };
+
   useEffect(fetchPrograms,[]);
   return (
     <div>
@@ -28,7 +38,7 @@ const ProgramList = () => {
             <button onClick={()=>console.log("Edit the program")}>
               Edit the program
             </button>
-            <button onClick={()=>console.log("Delete the program")}>
+            <button onClick={()=>deleteProgram(program.id)}>
               Delete the program
             </button>
           </div>
