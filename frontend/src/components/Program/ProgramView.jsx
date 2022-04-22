306028350bc051c1310ae5f2b28d1e58d9132a7d
Anh <tongkimanh@yahoo.com>
2022-04-22 14:20:59 -0400

    To work on styling Program View page

--- frontend/src/components/Program/ProgramView.jsx
+++ frontend/src/components/Program/ProgramView.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useProgramData } from '../../hooks/useProgramData';
 import { FaPlusCircle, FaTrash, FaEdit} from "react-icons/fa";
-import { Container, Row, Col, Button, Image, ButtonGroup, Stack } from 'react-bootstrap'
+import { Container, Row, Col, Button, Image} from 'react-bootstrap'
 import '../styles/program.css';
 import Confirm from './Confirm';
 
@@ -18,53 +18,22 @@ const ProgramView = () => {
 
   return (
     <div className="programview">
-      <Stack direction="horizontal" gap={3} style={{margin: "50px"}}>
-        <Stack gap={4}>
-          <h3> Welcome to {program.name} program! </h3>
-          <p> {program.description}</p>
-          <p> Languages: HTML, CSS, JavaScript, SQL... </p>
-          <p> Duration: {program.duration_days} days.</p>
-        </Stack>
-        <Image
-          src={program.img_url.url}
-          alt={program.name}
-          height={"300px"}
-          width={"300px"}
-        />
-        <Stack gap={4} style={{width: "200px"}}>
-          <Button variant={"primary"}
-            onClick={()=>navigate(`/program/edit/${program.id}`)}>
-            <FaEdit /> Edit Program
-          </Button>
-          <Button variant={"danger"} onClick={toggleModal}>
-            <FaTrash/> Delete Program
-          </Button>
-          <Button onClick={()=>navigate("/program/new")} variant={"secondary"}>
-            <FaPlusCircle/> Add a New Program
-          </Button>
-          <Button onClick={()=>navigate(programRoot)} variant={"info"}>
-            View All Programs
-          </Button>
-        </Stack>       
-      </Stack>
-
       <Container fluid>
-        <Row>
-          <Col>
+        <Row style={{height: "600px"}}>
+          <Col md={6}>
               <h3> Welcome to {program.name} program! </h3>
               <p> {program.description}</p>
               <p> Languages: HTML, CSS, JavaScript, SQL... </p>
               <p> Duration: {program.duration_days} days.</p>
           </Col>
-          <Col>
+          <Col md={4}>
             <Image
               src={program.img_url.url}
               alt={program.name}
-              height={"300px"}
-              width={"300px"}
+              height={"60%"}
             />
           </Col>
-          <Col className='buttonGroup'>
+          <Col className='buttonGroup' md={2} style={{minWidth: "200px"}}>
             <Button variant={"primary"}
             onClick={()=>navigate(`/program/edit/${program.id}`)}>
             <FaEdit /> Edit Program
