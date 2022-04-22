882761dd98a6693997071decfefb94547541e3dd
Anh <tongkimanh@yahoo.com>
2022-04-22 12:56:06 -0400

    To work on styling Program View page

--- frontend/src/components/Program/ProgramView.jsx
+++ frontend/src/components/Program/ProgramView.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useProgramData } from '../../hooks/useProgramData';
 import { FaPlusCircle, FaTrash, FaEdit} from "react-icons/fa";
-import { Row, Col, Button, Image, ButtonGroup } from 'react-bootstrap'
+import { Container, Row, Col, Button, Image, ButtonGroup, Stack } from 'react-bootstrap'
 import '../styles/program.css';
 import Confirm from './Confirm';
 
@@ -18,6 +18,71 @@ const ProgramView = () => {
 
   return (
     <div className='programView'>
+      <Stack direction="horizontal" gap={3}>
+        <div>
+          <h3> Welcome to {program.name} program! </h3>
+          <p> {program.description}</p>
+          <p> Languages: HTML, CSS, JavaScript, SQL... </p>
+          <p> Duration: {program.duration_days} days.</p>
+        </div>
+        <div>
+          <img
+            src={program.img_url.url}
+            alt={program.name}
+            max-height={"100%"}
+            max-width={"200px"}
+            />
+        </div>
+        <Stack gap={4}>
+          <Button variant={"primary"}
+            onClick={()=>navigate(`/program/edit/${program.id}`)}>
+            <FaEdit /> Edit Program
+          </Button>
+          <Button variant={"danger"} onClick={toggleModal}>
+            <FaTrash/> Delete Program
+          </Button>
+          <Button onClick={()=>navigate("/program/new")} variant={"secondary"}>
+            <FaPlusCircle/> Add a New Program
+          </Button>
+          <Button onClick={()=>navigate(programRoot)} variant={"info"}>
+            View All Programs
+          </Button>
+        </Stack>       
+      </Stack>
+
+      <Container fluid>
+        <Row>
+          <Col>
+              <h3> Welcome to {program.name} program! </h3>
+              <p> {program.description}</p>
+              <p> Languages: HTML, CSS, JavaScript, SQL... </p>
+              <p> Duration: {program.duration_days} days.</p>
+          </Col>
+          <Col>
+            <Image
+              src={program.img_url.url}
+              alt={program.name}
+              max-height={"100%"}
+              max-width={"200px"}
+            />
+          </Col>
+          <Col>
+            <Button variant={"primary"}
+            onClick={()=>navigate(`/program/edit/${program.id}`)}>
+            <FaEdit /> Edit Program
+            </Button>
+            <Button variant={"danger"} onClick={toggleModal}>
+              <FaTrash/> Delete Program
+            </Button>
+            <Button onClick={()=>navigate("/program/new")} variant={"secondary"}>
+              <FaPlusCircle/> Add a New Program
+            </Button>
+            <Button onClick={()=>navigate(programRoot)} variant={"info"}>
+              View All Programs
+            </Button>
+          </Col>
+        </Row>
+      </Container>
       <div>
         <h3> Welcome to {program.name} program! </h3>
         <p> {program.description}</p>
@@ -25,14 +90,12 @@ const ProgramView = () => {
         <p> Duration: {program.duration_days} days.</p>
       </div>
       <div>
-        <Image
+        <img
           src={program.img_url.url}
           alt={program.name}
-          height={'100%'}
           />
       </div>
-      <div className="buttonGroup">
-
+      <div>
         <Button variant={"primary"}
           onClick={()=>navigate(`/program/edit/${program.id}`)}>
           <FaEdit /> Edit Program
@@ -46,7 +109,6 @@ const ProgramView = () => {
         <Button onClick={()=>navigate(programRoot)} variant={"info"}>
           View All Programs
         </Button>
-
       </div>
       <Confirm 
         isOpen={isOpen}
