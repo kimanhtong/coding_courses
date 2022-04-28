b7a30b16fa4a4e0fac09b04ec7e23ab888486447
Anh <tongkimanh@yahoo.com>
2022-04-28 13:28:13 -0400

    To add cypress test

--- cypress/integration/1-program/program_list_spec.js
+++ cypress/integration/1-program/program_list_spec.js
@@ -2,4 +2,8 @@ describe('The Program List Page', () => {
   it('successfully loads', () => {
     cy.visit('/program') // change URL to match your dev URL
   })
+  it('successfully loads create page', () => {
+    cy.visit('/program')
+    cy.contains('Add a New Program').click()
+  })
 })
