732aed3616818c67b2cb2624d34faf2bd55217dc
Anh <tongkimanh@yahoo.com>
2022-04-28 15:11:04 -0400

    To add cypress test

--- cypress/integration/1-program/program_list_spec.js
+++ cypress/integration/1-program/program_list_spec.js
@@ -1,10 +1,12 @@
 describe('The Program Page', () => {
   it('Program List page loaded', () => {
     cy.visit('/program') // change URL to match your dev URL
+    cy.contains('Welcome to the Program List page!').should('be.visible')
   })
   it('Program Create page loaded', () => {
     cy.visit('/program')
     cy.contains('Add a New Program').click()
+    cy.contains('Create new program').should('be.visible')
   })
   it('Add a new Program successfully', () => {
     cy.visit('/program')
@@ -12,7 +14,8 @@ describe('The Program Page', () => {
     cy.get('input[name=name]').type('Lighthouse Labs')
     cy.get('input[name=duration_days]').type('180')
     cy.get('textarea[name=description]').type('The Cypress team prides itself on the ease of its install process. Remember that we need to make the right things easy. If it is hard to install the test framework, then it is less likely developers will go through all of the trouble.')
-    cy.get('input[type=file]').click()
-
+    cy.get('input[type=file]').selectFile('LHLpic1.png')
+    cy.contains('Submit').click()
+    cy.contains('Welcome to Lighthouse Labs program!').should('be.visible')
   })
 })
