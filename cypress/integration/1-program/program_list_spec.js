bacf2db7db437351b3c593d388b80d291c0a999a
Anh <tongkimanh@yahoo.com>
2022-04-28 14:07:41 -0400

    To add cypress test

--- cypress/integration/1-program/program_list_spec.js
+++ cypress/integration/1-program/program_list_spec.js
@@ -1,9 +1,18 @@
-describe('The Program List Page', () => {
-  it('successfully loads', () => {
+describe('The Program Page', () => {
+  it('Program List page loaded', () => {
     cy.visit('/program') // change URL to match your dev URL
   })
-  it('successfully loads create page', () => {
+  it('Program Create page loaded', () => {
     cy.visit('/program')
     cy.contains('Add a New Program').click()
+  })
+  it('Add a new Program successfully', () => {
+    cy.visit('/program')
+    cy.contains('Add a New Program').click()
+    cy.get('input[name=name]').type('Lighthouse Labs')
+    cy.get('input[name=duration_days]').type('180')
+    cy.get('textarea[name=description]').type('The Cypress team prides itself on the ease of its install process. Remember that we need to make the right things easy. If it is hard to install the test framework, then it is less likely developers will go through all of the trouble.')
+    cy.get('input[type=file]').click()
+
   })
 })
