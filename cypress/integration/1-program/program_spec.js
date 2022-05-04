da1747893891ed2d8e96e2809234105b1bc5f963
Anh <tongkimanh@yahoo.com>
2022-05-03 20:06:51 -0400

    Learning to implement code coverage measurement

--- cypress/integration/1-program/program_spec.js
+++ cypress/integration/1-program/program_spec.js
@@ -102,7 +102,7 @@ describe('Program pages - Basic CRUD actions', () => {
   beforeEach(function () {
     cy.visit('/program')
   })
-  it('Add a new Program successfully', () => {
+  it('Add and Delete a new Program successfully', () => {
     cy.contains('Add a New Program').click()
     cy.get('input[name=name]').type('Lighthouse Labs')
     cy.get('input[name=duration_days]').type('180')
@@ -112,8 +112,23 @@ describe('Program pages - Basic CRUD actions', () => {
     cy.contains('Welcome to Lighthouse Labs program!').should('be.visible')
     cy.contains('View All Programs').click()
     cy.contains('.card', 'Lighthouse Labs').should('be.visible')
+    cy.contains('.card', 'Lighthouse Labs') 
+    .children('.card-footer')
+    .children('button.btn.btn-danger')
+    .click()
+    cy.contains('Yes').click()
+    cy.get('.card-title').contains('Lighthouse Labs').should('not.exist')
   })
-  it('View an existent Program successfully', () => {
+  it('View and Edit an existent Program successfully', () => {
+    cy.contains('Add a New Program').click()
+    cy.get('input[name=name]').type('Lighthouse Labs')
+    cy.get('input[name=duration_days]').type('180')
+    cy.get('textarea[name=description]').type('The Cypress team prides itself on the ease of its install process. Remember that we need to make the right things easy. If it is hard to install the test framework, then it is less likely developers will go through all of the trouble.')
+    cy.get('input[type=file]').selectFile('cypress/fixtures/LHLpic1.png')
+    cy.contains('Submit').click()
+    cy.contains('Welcome to Lighthouse Labs program!').should('be.visible')
+    cy.contains('View All Programs').click()
+    cy.contains('.card', 'Lighthouse Labs').should('be.visible')
     cy.contains('.card', 'Lighthouse Labs') 
       .children('.card-footer')
       .children('button.btn.btn-info')
@@ -123,12 +138,7 @@ describe('Program pages - Basic CRUD actions', () => {
     cy.contains('180').should('be.visible')
     cy.contains('The Cypress team prides itself on the ease of its install process. Remember that we need to make the right things easy. If it is hard to install the test framework, then it is less likely developers will go through all of the trouble.').should('be.visible')
     cy.get('img').should('be.visible')
-  })
-  it('Edit an existent program', () => {
-    cy.contains('.card', 'Lighthouse Labs')
-    .children('.card-footer')
-    .children('button.btn.btn-secondary')
-    .click()
+    cy.contains('Edit Program').click()
     cy.get('input[name=name]').clear().type('Web Flex Bootcamp')
     cy.get('input[name=duration_days]').clear().type('270')
     cy.get('textarea[name=description]').clear().type('Every test you write will include selectors for elements. To save yourself a lot of headaches, you should write selectors that are resilient to changes by: \nDon\'t target elements based on CSS attributes such as: id, class, tag \nDon\'t target elements that may change their textContent \nAdd data-* attributes to make it easier to target elements')
@@ -137,13 +147,13 @@ describe('Program pages - Basic CRUD actions', () => {
     cy.contains('Welcome to Web Flex Bootcamp program!').should('be.visible')
     cy.get('h1').contains('Lighthouse Labs').should('not.exist')
   })
-  it('Delete an existent Program successfully', () => {
+  after(() => {
+    cy.visit('/program')
     cy.contains('.card', 'Web Flex Bootcamp') 
     .children('.card-footer')
     .children('button.btn.btn-danger')
     .click()
     cy.contains('Yes').click()
-    cy.get('.card-title').contains('Web Flex Bootcamp').should('not.exist')
   })
 })
 
